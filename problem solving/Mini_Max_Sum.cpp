1d954fa55e1c099497b4d4e00424635128a02e3d
priyam shankar <priyamshankar.5@gmail.com>
2021-07-28 20:53:54 +0530

    adding min max sum

---
+++ problem solving/Mini_Max_Sum.cpp
@@ -0,0 +1,42 @@
+
+
+#include <iostream>
+using namespace std;
+int main(){
+int arr[7];
+int small,large,i,s;
+s=0;
+for (i=0;i<5;i++){
+    
+    cin>>arr[i];
+    
+}
+for (i=0;i<5;i++){
+    
+   s=arr[i]+s;
+   
+    
+}
+
+small=arr[0];
+large=arr[0];
+
+for (i=1;i<5;i++){
+    if (arr[i]>large){
+        large=arr[i];
+        
+    }
+    
+   
+}
+for (i=1;i<5;i++){
+    if (arr[i]<small){
+        small=arr[i];
+       
+    }
+   
+}
+cout<<(s-large)<<" "<<(s-small);
+
+    return 0;
+}
---
+++ problem solving/tempCodeRunnerFile.cpp
@@ -0,0 +1,23 @@
+small=arr[0];
+// large=arr[0];
+
+// for (i=1;i<5;i++){
+//     if (arr[i]>large){
+//         large=arr[i];
+        
+//     }
+    
+   
+// }
+// for (i=1;i<5;i++){
+//     if (arr[i]<small){
+//         small=arr[i];
+       
+//     }
+   
+// }
+// cout<<s<<endl;
+// cout<<(s-small)<<" \n"<<(s-large);
+
+//     return 0;
+// }
